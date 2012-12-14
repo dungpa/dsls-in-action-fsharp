4e2a2c73fde4e88881a67ef01d98ae8bc12bc3cd
Phan Anh Dung <phananhdung309@yahoo.com>
2012-12-14 17:24:57 +0100

    Add a draft for Account and Trade
    All the modules are still messed up.

---
+++ Chapter6/Account.Scala.fs
@@ -0,0 +1,27 @@
+﻿module FSharpx.Books.DSLsInAction.Chapter4.Scala.Account
+
+open System
+
+// Listing 6.3 Account model in F#
+
+type AccountType = AccountType of string
+
+let CLIENT = AccountType("Client")
+let BROKER = AccountType("Broker")
+
+type Account(no: string, name: string, openDate: DateTime) =    
+    let mutable closeDate = DateTime.Today
+    let mutable creditLimit = Convert.ToDecimal(100000)
+    member x.AccountType = Unchecked.defaultof<AccountType>
+    member x.Close(date: DateTime) = closeDate <- date
+        
+type ClientAccount(no, name, ?openDate) =
+    inherit Account(no, name, defaultArg openDate DateTime.Today)
+    member x.AccountType = CLIENT
+
+type BrokerAccount(no, name, ?openDate) =
+    inherit Account(no, name, defaultArg openDate DateTime.Today)
+    member x.AccountType = BROKER
+
+let NOMURA = ClientAccount("Nom-123", "Nomura")
+
---
+++ Chapter6/Trade.Scala.fs
@@ -0,0 +1,31 @@
+﻿module FSharpx.Books.DSLsInAction.Chapter4.Scala.Trade
+
+open System
+
+open Account
+open Instrument
+
+// Listing 6.4 Trade model in F#
+
+[<AbstractClass>]
+type Trade =
+    abstract member TradingAccount: Account
+    abstract member Instrument: Instrument
+    abstract member Currency: Currency
+    abstract member TradeDate: DateTime
+    abstract member UnitPrice: decimal
+    abstract member Quantity: decimal
+    abstract member Market: string // Market
+    abstract member CashValue: decimal
+    abstract member Taxes: Map<string, decimal> // TaxFee
+
+[<AbstractClass>]
+type FixedIncomeTrade =
+    inherit Trade
+    abstract member Instrument: FixedIncome
+    abstract member AccruedInterest: decimal
+
+[<AbstractClass>]
+type EquityTrade =
+    inherit Trade
+    abstract member Instrument: Equity
