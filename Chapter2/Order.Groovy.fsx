356323f8090dcce12ec371fbbc7d3e8461288aab
Phan Anh Dung <phananhdung309@yahoo.com>
2012-12-14 19:55:26 +0100

    Update Mono solution

--- Chapter2/Order.Groovy.fsx
+++ Chapter2/Order.Groovy.fsx
@@ -2,7 +2,6 @@
 
 open FSharpx.Books.DSLsInAction.Chapter2.Groovy
 
-/// There is a minor limitation due to 100.Shares is parsed as an identifier by F#
 let order = 
     NewOrder.To.Buy(100 .Shares.Of "IBM") {
         limitPrice = 300
