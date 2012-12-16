4157986e3094704d75a3902955ecc0c206746d96
Phan Anh Dung <phananhdung309@yahoo.com>
2012-12-16 18:13:20 +0100

    Add first FParsec example in Chapter 8

---
+++ Chapter8/Parsing.Trading.Scala.fs
@@ -0,0 +1,41 @@
+﻿module FSharpx.Books.DSLsInAction.Chapter8.Scala.Trading.Parsing
+
+open FParsec
+open FParsec.Primitives 
+open FParsec.CharParsers 
+
+open System
+
+// Listing 8.2 A sample external DSL using Parser Combinators
+
+type Parser<'a> = Parser<'a, unit>
+
+let ws = spaces
+let str = pstring
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+
+let identifier = many1SatisfyL isLetter "identifier"
+let stringLit = betweenStrings "\"" "\"" (manySatisfy ((<>) '"'))
+let account = str "for" .>>. ws .>>. str "account" .>>. ws >>. stringLit
+
+let min_max = str "min" <|> str "max"
+let numeral = many1SatisfyL isDigit "digit" |>> Convert.ToInt32
+let price = tuple2 (str "at" >>. ws >>. min_max .>> ws) numeral
+
+let security = tuple2 (numeral .>> ws) (identifier .>> ws .>> str "shares")
+
+let buy_sell = str "to" >>. ws >>. (str "buy" <|> str "sell")
+
+let line_item = tuple3 (security .>> ws) (buy_sell .>> ws) price
+
+let items = betweenStrings "(" ")" (sepEndBy1 line_item (str "," .>> ws))
+
+let order: Parser<_> = tuple2 (items .>> ws) account
+
+let parseTradings str = 
+    match run order str with
+    | Success(result, _, _)   -> result
+    | Failure(errorMsg, _, _) -> failwithf "Failure: %s from \"%s\"" errorMsg str
+
+
+
---
+++ Chapter8/Parsing.Trading.Scala.fsx
@@ -0,0 +1,14 @@
+﻿#I @"..\packages\FParsec.0.9.2.0\lib\net40"
+#r "FParsecCS"
+#r "FParsec"
+
+#load "Parsing.Trading.Scala.fs"
+
+open FSharpx.Books.DSLsInAction.Chapter8.Scala.Trading.Parsing
+
+let example = """(100 IBM shares to buy at max 45, 40 Sun shares 
+      to sell at min 24, 25 CISCO shares to buy at max 56) 
+      for account "A1234"
+      """
+
+let result = parseTradings example
