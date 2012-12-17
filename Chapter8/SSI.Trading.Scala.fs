fc68e0e77f6d7df06dc3e34386cdf2c3874293d2
Phan Anh Dung <phananhdung309@yahoo.com>
2012-12-17 16:32:22 +0100

    Add an example with PEG grammar

---
+++ Chapter8/SSI.Trading.Scala.fs
@@ -0,0 +1,32 @@
+﻿namespace FSharpx.Books.DSLsInAction.Chapter8.Scala.Trading.SSI
+
+module AST =
+    type Market = string
+    type Security = string
+    type CustomerCode = string
+    type BrokerCode = string
+    type AccountNo = string
+    type Bank = string
+
+    type SettlementModeRule =
+        | SettleInternal of AccountNo
+        | SettleExternal of Bank * AccountNo
+
+    type SettleCashSecurityRule =
+        | SettleCash
+        | SettleSecurity
+
+    type SettlementRule =
+        | SettleCashSecuritySeparate of (SettleCashSecurityRule * SettlementModeRule) list
+        | SettleAll of SettlementModeRule
+
+    type CounterpartyRule = 
+        | Customer of CustomerCode
+        | Broker of BrokerCode
+
+    type TradeTypeRule = TradeTypeRule of Market option * Security option * AccountNo option
+
+    type StandingRule = StandingRule of TradeTypeRule * SettlementRule
+
+    type StandingRules = StandingRules of StandingRule list
+
---
+++ Chapter8/SSI.Trading.Scala.fsx
@@ -0,0 +1,10 @@
+﻿#I @"../packages/FParsec.0.9.2.0/lib/net40"
+#r "FParsecCS"
+#r "FParsec"
+
+#load "SSI.Trading.Scala.fs"
+
+open FSharpx.Books.DSLsInAction.Chapter8.Scala.Trading.SSI
+
+let example =  """settle trades for broker "icici" in "JPN" market safekeep security internally with us at "us-123" settle cash externally at "BOJ" "b-954"
+"""
