b33499f9e057141575ba2560145af4343bec7043
Phan Anh Dung <phananhdung309@yahoo.com>
2012-12-18 17:37:21 +0100

    Update Account model

--- Chapter6/Account.Scala.fs
+++ Chapter6/Account.Scala.fs
@@ -9,19 +9,19 @@ type AccountType = AccountType of string
 let CLIENT = AccountType("Client")
 let BROKER = AccountType("Broker")
 
+[<AbstractClass>]
 type Account(no: string, name: string, openDate: DateTime) =    
     let mutable closeDate = DateTime.Today
     let mutable creditLimit = Convert.ToDecimal(100000)
-    member x.AccountType = Unchecked.defaultof<AccountType>
+    abstract member AccountType: AccountType
     member x.Close(date: DateTime) = closeDate <- date
         
 type ClientAccount(no, name, ?openDate) =
     inherit Account(no, name, defaultArg openDate DateTime.Today)
-    member x.AccountType = CLIENT
+    override x.AccountType = CLIENT
 
 type BrokerAccount(no, name, ?openDate) =
     inherit Account(no, name, defaultArg openDate DateTime.Today)
-    member x.AccountType = BROKER
+    override x.AccountType = BROKER
 
 let NOMURA = ClientAccount("Nom-123", "Nomura")
-
