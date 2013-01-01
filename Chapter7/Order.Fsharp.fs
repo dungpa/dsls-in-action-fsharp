831c40d251c72d07ec151268d3246b17196e84b9
Phan Anh Dung <phananhdung309@yahoo.com>
2013-01-01 19:42:31 +0100

    A DSL by query expression in Chapter 7
    Beware that this DSL isn't a monad

---
+++ Chapter7/Order.Fsharp.fs
@@ -0,0 +1,37 @@
+﻿module FSharpx.Books.DSLsInAction.Chapter7.Fsharp.Order
+
+open System.Text
+
+type BuySell = Buy | Sell
+type At = At
+type For = For
+type PriceType = Price | LimitPrice
+
+type Order (buySell: BuySell, security: string, price: int, account: string) = 
+    member x.BuySell = buySell
+    member x.Security = security
+    member x.Price = price
+    member x.Account = account
+
+    static member Create(buySell, security, price, account) =
+        Order(buySell, security, price, account)
+
+    override x.ToString() =
+        sprintf "Order is {%A} / {%A} / {%A} / {%A}" x.BuySell x.Security x.Price x.Account
+                     
+type OrderBuilder() =
+    member x.Yield (()) = Seq.empty<Order>
+
+    [<CustomOperation("buy")>]
+    member x.Buy (source : seq<_>, s: string, a: At, p: PriceType, i: int, f: For, acc: string) : seq<Order> =
+        seq { yield! source
+              yield Order(Buy, s, i, acc) }
+  
+    [<CustomOperation("sell")>]
+    member x.Sell (source : seq<_>, s: string, a: At, p: PriceType, i: int, f: For, acc: string) : seq<Order> =
+        seq { yield! source
+              yield Order(Sell, s, i, acc) }
+  
+let order = OrderBuilder()
+
+
---
+++ Chapter7/Order.Fsharp.fsx
@@ -0,0 +1,10 @@
+﻿#load "Order.Fsharp.fs"
+
+open FSharpx.Books.DSLsInAction.Chapter7.Fsharp.Order
+
+let order1 = 
+    order {
+        buy "IBM" At Price 100 For "NOMURA"
+        sell "GOOGLE" At LimitPrice 70 For "CHASE"
+    }
+    |> Seq.toList
