57358ea85504449c2d7b89c29a62f26dca8077ad
Phan Anh Dung <phananhdung309@yahoo.com>
2013-01-01 20:25:06 +0100

    Add another query expression
    Unfortunately, custom operations cannot be overloaded.

---
+++ Chapter8/Semantic.Trading.Fsharp.fs
@@ -0,0 +1,38 @@
+﻿module FSharpx.Books.DSLsInAction.Chapter8.Fsharp.Trading.Semantic
+
+type PriceType = Min | Max
+    
+type Price = Price of PriceType * int
+
+type Security = Security of int * string
+
+type BuySell = Buy | Sell
+
+type LineItem = LineItem of Security * BuySell * Price
+
+type Items = Items of LineItem list
+
+type Account = Account of string
+
+type Order = Order of Items * Account
+
+type At = At
+type Shares = Shares
+
+type TradeBuilder() =
+    member x.Yield (()) = Items []
+
+    [<CustomOperation("buy")>]
+    member x.Buy (Items sources, i: int, s: string, sh: Shares, a: At, m: PriceType, p: int) : Items =
+        Items [ yield! sources
+                yield LineItem(Security(i, s), Buy, Price(m, p)) ]
+  
+    [<CustomOperation("sell")>]
+    member x.Sell (Items sources, i: int, s: string, sh: Shares, a: At, m: PriceType, p: int) : Items =
+        Items [ yield! sources
+                yield LineItem(Security(i, s), Sell, Price(m, p)) ]  
+
+let trade = TradeBuilder()
+
+let inline applyFor constr s items = Order(items, constr s)
+
---
+++ Chapter8/Semantic.Trading.Fsharp.fsx
@@ -0,0 +1,12 @@
+﻿#load "Semantic.Trading.Fsharp.fs"
+
+open FSharpx.Books.DSLsInAction.Chapter8.Fsharp.Trading.Semantic
+
+let example = 
+    trade {
+        buy 100 "IBM" Shares At Max 45
+        sell 40 "Sun" Shares At Min 24
+        buy 25 "CISCO" Shares At Max 56 
+    }
+    |> applyFor Account "A1234"
+
